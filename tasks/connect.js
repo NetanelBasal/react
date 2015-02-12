30a776f563b2c62c73820830ab440450fcbea6b7
netanelbasal <netanel7799@gmail.com>
2015-02-12 18:23:25 +0200

    update package json

--- tasks/connect.js
+++ tasks/connect.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var browserSync = require('browser-sync');
 
 var config = require('./../gulp-config');
 
-
 gulp.task('connect', function() {
   connect.server({
-    root: config.server.root,
+    root      : config.server.root,
     livereload: true
   });
 });
